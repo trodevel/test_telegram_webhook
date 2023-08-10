2dd4098a71ff0e8739374f4ab2c1de196b22a53c
trodevel <trodevel@gmail.com>
2023-08-10 16:11:58 +0200

    simplified

--- test.py
+++ test.py
@@ -24,17 +24,11 @@ from aiohttp import web
 import ssl
 
 WEBHOOK_LISTEN = "0.0.0.0"
-WEBHOOK_PORT = config.PORT
 
-WEBHOOK_SSL_CERT = config.CERTIFICATE_PATH
-WEBHOOK_SSL_PRIV = config.PRIVATE_KEY_PATH
-
-API_TOKEN = config.TOKEN
-bot = telebot.TeleBot(API_TOKEN)
+bot = telebot.TeleBot( config.TOKEN )
 
 app = web.Application()
 
-# process only requests with correct bot token
 async def handle(request: web.Request ) -> web.Response:
     print("handle")
     request_body_dict = await request.json()
@@ -57,29 +51,24 @@ app.add_routes([web.get("/", handle),web.post("/", handle2)])
 #app.router.add_get("/", handle2)
 
 help_string = []
-help_string.append("*Some bot* - just a bot.\n\n")
+help_string.append("*Test bot* - just a bot.\n\n")
 help_string.append("/start - greetings\n")
 help_string.append("/help - shows this help")
 
-# - - - messages
-
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Ololo, I am a bot")
+    bot.send_message(message.chat.id, "Hello, I am a bot")
 
 @bot.message_handler(commands=["help"])
 def send_help(message):
     bot.send_message(message.chat.id, "".join(help_string), parse_mode="Markdown")
 
-# - - -
-
 context = ssl.SSLContext()
-context.load_cert_chain(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV)
+context.load_cert_chain( config.CERTIFICATE_PATH, config.PRIVATE_KEY_PATH )
 
-# start aiohttp server (our bot)
 web.run_app(
     app,
     host=WEBHOOK_LISTEN,
-    port=WEBHOOK_PORT,
+    port=config.PORT,
     ssl_context=context,
 )
