a270240e9f8bdc18a62dda77be278c854ab2827e
trodevel <trodevel@gmail.com>
2023-08-10 16:31:42 +0200

    First try (#1)
    * initial commit
    
    * added INSTALL
    
    * CFG: added sample config
    
    * initial commit
    
    * updated .gitignore
    
    * added removed config.py
    
    * added boilerplate
    
    * simplified
    
    * intermediate results
    
    * simplified
    
    * cosmetics

---
+++ test.py
@@ -0,0 +1,71 @@
+'''
+Test Telegram Webhook.
+
+Copyright (C) 2023 Dr. Sergey Kolevatov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+
+import config
+import telebot
+from aiohttp import web
+import ssl
+
+bot = telebot.TeleBot( config.TOKEN )
+
+app = web.Application()
+
+async def handle( request: web.Request ) -> web.Response:
+    print("handle")
+    request_body_dict = await request.json()
+    update = telebot.types.Update.de_json(request_body_dict)
+    bot.process_new_updates([update])
+    return web.Response()
+
+async def handle2(request: web.Request ) -> web.Response:
+    print("handle2")
+    request_body_dict = await request.json()
+    update = telebot.types.Update.de_json(request_body_dict)
+    bot.process_new_updates([update])
+    return web.Response(text="Hello, world")
+
+
+app.add_routes([web.get("/", handle),web.post("/", handle2)])
+
+#app.router.add_post("/", handle)
+
+#app.router.add_get("/", handle2)
+
+hello_string = "*Test bot* - just a bot.\n\n" \
+               "/start - greetings\n" \
+               "/help - shows this help"
+
+@bot.message_handler( commands=["start"] )
+def send_welcome( message ):
+    bot.send_message( message.chat.id, "Hello, I am a bot" )
+
+@bot.message_handler( commands=["help"] )
+def send_help( message ):
+    bot.send_message( message.chat.id, hello_string, parse_mode="Markdown" )
+
+context = ssl.SSLContext()
+context.load_cert_chain( config.CERTIFICATE_PATH, config.PRIVATE_KEY_PATH )
+
+web.run_app(
+    app,
+    host=config.LISTEN_IP,
+    port=config.PORT,
+    ssl_context=context,
+)
